ee2d07f6d629ae57df5cadb219738b139aead918
bangari-19 <83721061+bangari-19@users.noreply.github.com>
2023-08-25 13:12:25 -0400

    Add files via upload
    tested code for individual 1, need to add clip_outliers function

--- sim_modules.R
+++ sim_modules.R
@@ -1,141 +1,134 @@
-# install and load required packages
-install.packages('plot.matrix')
-install.packages('matlib')
-install.packages('MASS')
-install.packages('stats')
+# SIM_MODULES
 
-library('plot.matrix')
-library('matlib')
-library('MASS')
-library('stats')
-
-
-plot_matrix <- function(mat, save=FALSE, name) {
+plot_matrix <- function(mat, save=FALSE, name=NULL) {
+  library('plot.matrix')
   plot(mat)
   if (save) {
-    fname = paste0(name, '.png')
-    png(file = fname)
-    plot(mat)
-    dev.off()
+    plot(mat)  # create plot
+    png(file = name)  # save plot as png
+    dev.off()  # close plot 
   }
 }
 
-
 save_matrix <- function(mat, name) {
-  fname = paste0(name, '.csv')
-  write.csv(mat, file = fname)
+  write.csv(mat, file = name)
 }
 
 
 make_mask <- function(mat, contemp) {
-  size = dim(mat)[1]  # returns number of rows 
+  "If lagged then mask all zero entries 
+  If contemp then mask diagonal with zeros in addition"
+  
+  size = nrow(mat)  # number of rows 
   mask <- matrix(1, size, size)  
+  
   if (contemp) {
-    diag = rep(0, size)  # replicate element 0 of vector 'size' times
-    diag(mask) <- diag  # fill diagonal with zeros
-  } else {
-    indices = which(mat == 0, arr.ind = T)  # arr.ind outputs indices in row, col format
-    mask[indices] = 0  # indices where matrix = 0, make mask = 0 
+    diag(mask) <- 0  # fill diagonal of mask with zeros
   }
+  
+  inds = which(mat == 0, arr.ind = T)  # find indices where mat == 0 in (row, col) format
+  mask[inds] = 0  # make mask 0 at those indices 
+  
   return(mask)
 }
 
 
-# Draw a coefficient matrix from the mean matrix and the covariance
 coeff_draw_from_cov <- function(amplitude, mat, cov, mask) {
-  size <- dim(mat)[1]  
-  random_mat <- amplitude * matrix(rnorm(size**2), nrow = size)
-  sample_step <- cov %*% random_mat  # generate noise
-  step_mat <- mat + sample_step  # add noise 
-  noisy_mat <- step_mat %*% mask  # mask out terms 
+  size <- nrow(cov)
+  sample_step <- cov %*% (amplitude * matrix(rnorm(size * size), nrow = size, ncol = size))
+  stepmat <- mat + sample_step
+  noisy_mat <- stepmat * mask
   return(noisy_mat)
 }
 
 
-# Generate timeseries
-generate_timeseries <- function(start, len, contempamp, contempmat, contempcov, lagamp, lagmat, lagcov, measurecov, save=F){
-  size = length(start)  # 'start' is a vector 
-  samples = matrix(0, nrow=len, ncol=size)  # np.zeros((len, size))
-  samples[1, ] = start  # day 1 data, index starts at 1 in R
-  lagmask = make_mask(lagmat, contemp=FALSE)
-  contempmask = make_mask(contempmat, contemp=TRUE)
-  print(lagmask)
-  print(contempmask)
+generate_timeseries <- function(start, len, contempamp, contempmat, contempcov, lagamp, lagmat, lagcov, measurecov, save=FALSE) {
+  size <- length(start)  # length of 'start' vector
+  samples <- matrix(0, nrow = len, ncol = size)
+  samples[1,] <- start  # day 1 data (index start at 1 in R)
+  lagmask <- make_mask(lagmat, contemp=FALSE)
+  contempmask <- make_mask(contempmat, contemp=TRUE)
   
-  noisy_lagmat = coeff_draw_from_cov(lagamp, lagmat, lagcov, lagmask)
-  noisy_contempmat = coeff_draw_from_cov(contempamp, contempmat, contempcov, contempmask)
-  print(noisy_lagmat)
-  print(noisy_contempmat)
+  noisy_lagmat <- coeff_draw_from_cov(lagamp, lagmat, lagcov, lagmask)
+  noisy_contempmat <- coeff_draw_from_cov(contempamp, contempmat, contempcov, contempmask)
   
   if (save) {
-    save_matrix(noisy_lagmat, 'noisy_lagmat')
-    plot_matrix(noisy_lagmat, TRUE, 'noisy_lagmat')
-    save_matrix(noisy_contempmat, 'noisy_contempmat')
-    plot_matrix(noisy_contempmat, TRUE, 'noisy_contempmat')
+    save_matrix(noisy_lagmat, 'noisy_lagmat.csv')
+    plot_matrix(noisy_lagmat, TRUE, 'noisy_lagmat.png')
+    save_matrix(noisy_contempmat, 'noisy_contempmat.csv')
+    plot_matrix(noisy_contempmat, TRUE, 'noisy_contempmat.png')
   }
   
-  days <- c(2:len)  # days = (2, 3, ..., len)
-  for (i in days) {
-    Ylag_lagmat = samples[i-1, ] %*% noisy_lagmat 
-    inverse = inv(diag(size) - noisy_contempmat)  # diag(size) gives identity 
-    print(Ylag_lagmat)
-    print(inverse)
-    samples[i, ] = (Ylag_lagmat %*% inverse) 
-    print(samples[i, ])
-    measurement_noise <- mvrnorm(n = 1, mu = matrix(0, size, size), Sigma = measurecov)  # generates random samples from a multivariate normal distribution
-    print(measurement_noise)
-    samples[i, ] = samples[i, ] + measurement_noise
-    print(samples[i, ])
+  library('mvtnorm')
+  for (i in 2:len) {
+    samples[i,] <- samples[i-1,] %*% noisy_lagmat %*% solve(diag(size) - noisy_contempmat) + start
+    samples[i,] <- samples[i,] + rmvnorm(1, rep(0, size), measurecov)  
+    # rmvnorm generates one random sample from the multivar norm dist with mean 0 and covariance 'measurecov'
   }
-  #samples = samples 
-  samples = start[-1, ]  # get all data except for day 1 data  
   
-  return (samples)
+  samples <- samples[-1,] - start
+  return(samples)
 }
 
-# clip_timeseries
-clip_timeseries <- function(timeseries, indices_to_clip, min_vec, max_vec){
-  # copy timeseries matrix 
-  ts = matrix(timeseries, nrow=nrow(timeseries), ncol=ncol(timeseries))
+
+clip_timeseries <- function(timeseries, indices_to_clip, min_vec, max_vec) {
+  
+  ts <- timeseries  # copy of timeseries
+  
   for (i in indices_to_clip) {
-    min = min_vec[i]
-    max = max_vec[i]
-    min_inds = which(timeseries[, i] < min, arr.ind = T)
-    ts[min_inds, i] = min
-    max_inds = which(timeseries[, i] > max, arr.ind = T)
-    ts[max_inds, i] = max
+    min_val <- min_vec[i]
+    max_val <- max_vec[i]
+    
+    min_inds <- which(timeseries[, i] < min_val)  # indices where the ith column is less than min_val
+    ts[min_inds, i] <- min_val  # at those indices insert min_val
+    
+    max_inds <- which(timeseries[, i] > max_val)
+    ts[max_inds, i] <- max_val
   }
   
-  return (ts)
+  return(ts)
 }
 
-# clip_outliers
 
-# test for individual 1  
-data <- read.csv("~/Desktop/Astro/CIFAR Project/220429.FinalMatrices/1.csv")
+#TEST FOR INDIVIDUAL 1 
+data <- read.csv("~/Desktop/CIFAR/220429.FinalMatrices/1.csv")
+len = 10  # number of days for timeseries 
+start <- rep(0, 6)  # starting value for day 1
 size = 6
-start <- rep(0, size)
-steps = 100
-start_index = size+1
-stop = length(data)
-matContemp <- data[, start_index:stop]
-matLagged <- data[, 1:size]
+start_ind = size + 1
+stop_ind = length(data)
+matContemp <- data[, start_ind : stop_ind]
+matLagged <- data[, 1 : size]
+matContemp <- as.matrix(matContemp)  # convert from data format to matrix format
+matLagged <- as.matrix(matLagged)
+matContemp
+matLagged
 
-# covariances
-covContemp <- matrix(rnorm(size**2), nrow=size, ncol=size)
-covLagged <- matrix(rnorm(size**2), nrow=size, ncol=size)
+plot_matrix(matLagged)
+plot_matrix(matContemp)
+
+maskContemp = make_mask(matContemp, TRUE)
+maskContemp
+maskLagged = make_mask(matLagged, FALSE)
+maskLagged
+
+covContemp = matrix(rnorm(size * size), nrow=size, ncol=size)
+covContemp
+covLagged = matrix(rnorm(size * size), nrow=size, ncol=size)
+covLagged
+
 ampContemp = 0.1
 ampLagged = 0.1
-ampMeasure = 1.0
-measureCov <- ampMeasure * diag(size)
+ampMeasure = 1
+covMeasure = ampMeasure * diag(size)
+covMeasure
 
-# masks
-maskContemp =  make_mask(matContemp, contemp=T)
-maskLagged = make_mask(matLagged, contemp=F)
+ts = generate_timeseries(start, len, ampContemp, matContemp, covContemp, ampLagged, matLagged, covLagged, covMeasure)
+ts  # variables on column and data for each day on rows 
 
-# clipping 
+clip_indices <- c(0,1)
+clip_mins <- c(0.5, 0.7)
+clip_maxs <- c(0.8,1.3)
 
-# generate timeseries
-samples = generate_timeseries(start, steps, ampContemp, matContemp, covContemp, ampLagged, matLagged, covLagged, measureCov, save=F)
-
-  
+ts_clip = clip_timeseries(ts, clip_indices, clip_mins, clip_maxs)
+ts_clip
